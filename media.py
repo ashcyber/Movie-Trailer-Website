bdf92db2e65bf1d88ab7c6e28344de33bf6c8200
Aishwarya Sharma <ishans632@gmail.com>
2018-03-22 23:06:17 +0530

    Format code according to PEP8 guidelines

--- media.py
+++ media.py
@@ -1,15 +1,15 @@
 import webbrowser
-
 # Here we declare the Movie class
+
+
 class Movie():
     # init function defines movies class variables
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_line, poster_image, trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_storyline
+        self.movie_line = movie_line
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-
-    # show trailers function open webbrowser with youtube trailer link 
+    # show trailers function open webbrowser with youtube trailer link
     def show_trailer(self):
         webbrowser.open(self.trailer)
